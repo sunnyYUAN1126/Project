1d3d2363a8b617f6cf73a413830637d8796a8297
洪琪媛 <sunny.e1.126@gmail.com>
2025-12-08 13:46:21 +0800

    12/8使用者圖片2

--- SecondhandBooks/frontend/Vue/src/App.vue
+++ SecondhandBooks/frontend/Vue/src/App.vue
@@ -93,6 +93,9 @@ function adminLogout() {
           <li @click="logout">登出</li>
           <li @click="showMemberAreaPage" class="d-flex align-items-center">
             <img v-if="userPicture" :src="userPicture" alt="User" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 8px; object-fit: cover;">
+            <div v-else style="width: 30px; height: 30px; border-radius: 50%; margin-right: 8px; background-color: lightblue; display: flex; justify-content: center; align-items: center;">
+              <i class="bi bi-person-fill" style="color: white;"></i>
+            </div>
             {{ userName }}
           </li>
         </template>
